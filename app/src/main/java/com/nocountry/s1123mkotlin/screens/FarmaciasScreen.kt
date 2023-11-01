934f065efe24c48f6561a422f0d77d8ec73a05ea
Diana cuta <105395005+dianaq14@users.noreply.github.com>
2023-11-01 15:12:14 -0500

    FarmaciasScreen.kt

--- app/src/main/java/com/nocountry/s1123mkotlin/screens/FarmaciasScreen.kt
+++ app/src/main/java/com/nocountry/s1123mkotlin/screens/FarmaciasScreen.kt
@@ -1,241 +1,70 @@
-package com.nocountry.s1123mkotlin.screens
+package com.nocountry.s1123mkotlin.farmacias
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Application
-import android.content.Context
-import android.content.pm.PackageManager
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.*
-import com.google.android.libraries.places.api.model.Place
-import com.nocountry.s1123mkotlin.farmacias.ApiService
-import com.nocountry.s1123mkotlin.farmacias.FarmaciasViewModel
-import com.nocountry.s1123mkotlin.farmacias.RouteResponse
-import com.nocountry.s1123mkotlin.farmacias.getRetrofit
 import com.nocuntry.s1123mkotlin.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
-
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
-fun FarmaciasScreen(
-    navController: NavController,
-    viewModel: FarmaciasViewModel
-) {
+fun FarmaciasScreen(navController: NavController) {
     val context = LocalContext.current
-    val application = context.applicationContext as Application
-    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-    val locationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
-
-    val coroutineScope = rememberCoroutineScope()
-
-    val mapView = remember { MapView(context) }
-    var map: GoogleMap? by remember { mutableStateOf(null) }
-
-    val locationPermissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-        if (isGranted) {
-            getLastLocationAndSearchFarmacias(fusedLocationClient, viewModel, context, coroutineScope, mapView)
-        } else {
-            // Handle permission denied
-        }
-    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
             .background(colorResource(id = R.color.fondo_sintomas))
     ) {
+
+        Spacer(modifier = Modifier
+            .height(20.dp))
+
+
+        // Título de la pantalla
         Text(
             text = "Farmacias Cercanas",
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-    }
-
-    val getNearbyPharmacies = {
-        val hasLocationPermission = locationPermissionState.hasPermission
-        if (hasLocationPermission) {
-            getLastLocationAndSearchFarmacias(fusedLocationClient, viewModel, context, coroutineScope, mapView)
-        } else {
-            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
-
-    if (locationPermissionState.hasPermission) {
-        Button(
-            onClick = { getNearbyPharmacies() },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            Text("Buscar Farmacias Cercanas")
-        }
-    } else {
-        Text(
-            text = "Necesitas permisos de ubicación para buscar farmacias cercanas.",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(20.dp)
         )
 
+        // Botón para abrir Google Maps
         Button(
-            onClick = { locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION) },
+            onClick = {
+                // Latitud y longitud de Buenos Aires, Argentina
+                val latitude = -34.6118
+                val longitude = -58.4173
+
+                val uri = "geo:$latitude,$longitude?q=$latitude,$longitude(Buenos Aires, Argentina)"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                intent.setPackage("com.google.android.apps.maps")
+
+                // Abre Google Maps
+                context.startActivity(intent)
+            },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .padding(16.dp)
         ) {
-            Text("Solicitar Permisos de Ubicación")
+            Text("Ver en Google Maps",
+                color= Color.White)
         }
     }
-
-    FarmaciasList(farmacias = viewModel.farmacias, context)
-}
-private fun getLastLocationAndSearchFarmacias(
-    fusedLocationClient: FusedLocationProviderClient,
-    viewModel: FarmaciasViewModel,
-    context: Context,
-    coroutineScope: CoroutineScope,
-    mapView: MapView
-) {
-    val permissionStatus = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-
-    if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            if (location != null) {
-                val latLng = LatLng(location.latitude, location.longitude)
-                coroutineScope.launch {
-                    val route = getRouteToPharmacies(latLng, context)
-                    if (route != null) {
-                        val farmacias = updateFarmaciasBasedOnRoute(route)
-                        viewModel.farmacias = farmacias
-                        mapView.getMapAsync { googleMap ->
-                            googleMap.clear()
-                            val userLocation = CameraUpdateFactory.newLatLngZoom(latLng, 15f)
-                            googleMap.moveCamera(userLocation)
-                        }
-                    }
-                }
-            } else {
-                // Handle the case where location cannot be obtained
-            }
-        }
-    } else {
-        // Handle lack of permissions
-    }
-}
-private suspend fun getRouteToPharmacies(latLng: LatLng, context: Context): RouteResponse? {
-    val apiKey = context.getString(R.string.openrouteservice_apikey)
-    val start = "${latLng.longitude},${latLng.latitude}"
-
-    val retrofit = getRetrofit(context)
-    val apiService = retrofit.create(ApiService::class.java)
-
-    val response = apiService.getRoute(apiKey, start, "Destination Coordinates Here")
-    if (response.isSuccessful) {
-        return response.body()
-    }
-    return null
-}
-
-private fun updateFarmaciasBasedOnRoute(route: RouteResponse?): List<Place> {
-    val farmacias = mutableListOf<Place>()
-    if (route != null) {
-        val coordinates = route.coordinates
-        if (coordinates != null) {
-            // Implement logic to find nearby pharmacies based on the route
-            // You can use the Google Places API or other services for this
-            // and add them to the farmacias list
-        }
-    }
-    return farmacias
-}
-
-@Composable
-fun FarmaciasList(farmacias: List<Place>, context: Context) {
-    LazyColumn {
-        items(farmacias) { farmacia ->
-            FarmaciaItem(farmacia, context)
-        }
-    }
-}
-
-@Composable
-fun FarmaciaItem(farmacia: Place, context: Context) {
-    val apiKey = context.getString(R.string.openrouteservice_apikey)
-    val latLng = LatLng(farmacia.latLng?.latitude ?: 0.0, farmacia.latLng?.longitude ?: 0.0)
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = farmacia.name ?: "Nombre Desconocido",
-                style = MaterialTheme.typography.h6
-            )
-
-            Text(
-                text = farmacia.address ?: "Dirección Desconocida",
-                style = MaterialTheme.typography.body2
-            )
-
-            MapViewComposable(latLng, apiKey, context)
-        }
-    }
-}
-
-@SuppressLint("MissingPermission")
-@Composable
-fun MapViewComposable(latLng: LatLng, apiKey: String, context: Context) {
-    var mapView by remember { mutableStateOf<MapView?>(null) }
-    val coroutineScope = rememberCoroutineScope()
-
-    DisposableEffect(Unit) {
-        mapView = MapView(context)
-        mapView?.onCreate(null)
-        mapView?.getMapAsync { googleMap ->
-            googleMap.isMyLocationEnabled = true
-            val userLocation = CameraUpdateFactory.newLatLngZoom(latLng, 15f)
-            googleMap.moveCamera(userLocation)
-        }
-
-        onDispose {
-            mapView?.onDestroy()
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        mapView?.onResume()
-    }
-
-    AndroidView({ mapView!! })
 }
